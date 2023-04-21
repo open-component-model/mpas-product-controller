85c8be025457faab476c2b7a3353175105240a8a
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2023-04-21 13:13:05 +0200

    added v3alpha1.ElementMeta

--- main.go
+++ main.go
@@ -91,6 +91,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "ProductDeployment")
 		os.Exit(1)
 	}
+	if err = (&controllers.ProductDeploymentPipelineReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "ProductDeploymentPipeline")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
