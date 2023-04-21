37bf824106e8ceae37c66b52b38830432b2dccca
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2023-04-21 12:16:37 +0200

    feat: add ProductDeploymentPipeline

--- controllers/productdeployment_controller.go
+++ controllers/productdeployment_controller.go
@@ -6,13 +6,23 @@ package controllers
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
+	"github.com/fluxcd/pkg/apis/meta"
+	"github.com/fluxcd/pkg/runtime/conditions"
+	"github.com/fluxcd/pkg/runtime/patch"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
 
-	mpasv1alpha1 "github.com/open-component-model/mpas-product-controller/api/v1alpha1"
+	"github.com/open-component-model/mpas-product-controller/api/v1alpha1"
 )
 
 // ProductDeploymentReconciler reconciles a ProductDeployment object
@@ -27,8 +37,111 @@ type ProductDeploymentReconciler struct {
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-func (r *ProductDeploymentReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+// Named return values: It's used for improving readability when dealing with the defer patch statement.
+func (r *ProductDeploymentReconciler) Reconcile(ctx context.Context, req ctrl.Request) (result ctrl.Result, err error) {
+	logger := log.FromContext(ctx)
+
+	logger.V(4).Info("starting reconcile loop for product deployment")
+
+	obj := &v1alpha1.ProductDeployment{}
+	if err := r.Get(ctx, req.NamespacedName, obj); err != nil {
+		if apierrors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+	}
+
+	var patchHelper *patch.Helper
+	if patchHelper, err = patch.NewHelper(obj, r.Client); err != nil {
+		return
+	}
+
+	// Always attempt to patch the object and status after each reconciliation.
+	defer func() {
+		// Patching has not been set up, or the controller errored earlier.
+		if patchHelper == nil {
+			return
+		}
+
+		if condition := conditions.Get(obj, meta.StalledCondition); condition != nil && condition.Status == metav1.ConditionTrue {
+			conditions.Delete(obj, meta.ReconcilingCondition)
+		}
+
+		// Check if it's a successful reconciliation.
+		// We don't set Requeue in case of error, so we can safely check for Requeue.
+		if err == nil {
+			// Remove the reconciling condition if it's set.
+			conditions.Delete(obj, meta.ReconcilingCondition)
+
+			// Set the return err as the ready failure message if the resource is not ready, but also not reconciling or stalled.
+			if ready := conditions.Get(obj, meta.ReadyCondition); ready != nil && ready.Status == metav1.ConditionFalse && !conditions.IsStalled(obj) {
+				err = errors.New(conditions.GetMessage(obj, meta.ReadyCondition))
+			}
+		}
+
+		// If still reconciling then reconciliation did not succeed, set to ProgressingWithRetry to
+		// indicate that reconciliation will be retried.
+		if conditions.IsReconciling(obj) {
+			reconciling := conditions.Get(obj, meta.ReconcilingCondition)
+			reconciling.Reason = meta.ProgressingWithRetryReason
+			conditions.Set(obj, reconciling)
+		}
+
+		// If not reconciling or stalled than mark Ready=True
+		if !conditions.IsReconciling(obj) &&
+			!conditions.IsStalled(obj) &&
+			err == nil {
+			conditions.MarkTrue(obj, meta.ReadyCondition, meta.SucceededReason, "Reconciliation success")
+		}
+		// Set status observed generation option if the component is stalled or ready.
+		if conditions.IsStalled(obj) || conditions.IsReady(obj) {
+			obj.Status.ObservedGeneration = obj.Generation
+		}
+
+		// Update the object.
+		if perr := patchHelper.Patch(ctx, obj); perr != nil {
+			err = errors.Join(err, perr)
+		}
+	}()
+
+	// Remove any stale Ready condition, most likely False, set above. Its value
+	// is derived from the overall result of the reconciliation in the deferred
+	// block at the very end.
+	conditions.Delete(obj, meta.ReadyCondition)
+
+	return r.reconcile(ctx, obj)
+}
+
+func (r *ProductDeploymentReconciler) reconcile(ctx context.Context, obj *v1alpha1.ProductDeployment) (ctrl.Result, error) {
+	logger := log.FromContext(ctx)
+
+	logger.Info("preparing to create pipeline objects")
+	// Create all the pipeline objects.
+	for _, pipeline := range obj.Spec.Pipelines {
+		pobj := &v1alpha1.ProductDeploymentPipeline{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      pipeline.Name,
+				Namespace: obj.Namespace,
+			},
+			Spec: v1alpha1.ProductDeploymentPipelineSpec{
+				Resource:      pipeline.Resource,
+				Localization:  pipeline.Localization,
+				Configuration: pipeline.Configuration,
+				TargetRole:    pipeline.TargetRole,
+			},
+		}
+
+		if err := controllerutil.SetOwnerReference(obj, pobj, r.Scheme); err != nil {
+			return ctrl.Result{}, fmt.Errorf("failed to set owner reference: %w", err)
+		}
+
+		if err := r.Create(ctx, pobj); err != nil {
+			return ctrl.Result{}, fmt.Errorf("failed to create pipeline object: %w", err)
+		}
+
+		logger.V(4).Info("pipeline object successfully created", "name", pobj.Name)
+	}
+
+	logger.Info("all pipeline objects successfully created")
 
 	return ctrl.Result{}, nil
 }
@@ -36,6 +149,6 @@ func (r *ProductDeploymentReconciler) Reconcile(ctx context.Context, req ctrl.Re
 // SetupWithManager sets up the controller with the Manager.
 func (r *ProductDeploymentReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&mpasv1alpha1.ProductDeployment{}).
+		For(&v1alpha1.ProductDeployment{}, builder.WithPredicates(predicate.GenerationChangedPredicate{})).
 		Complete(r)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/open-component-model/mpas-product-controller
 go 1.20
 
 require (
+	github.com/fluxcd/pkg/apis/meta v0.19.0
+	github.com/fluxcd/pkg/runtime v0.27.0
 	github.com/open-component-model/replication-controller v0.2.1-0.20230420172740-670e1238a8c7
 	k8s.io/api v0.26.1
 	k8s.io/apimachinery v0.26.1
@@ -17,7 +19,6 @@ require (
 	github.com/emicklei/go-restful/v3 v3.10.0 // indirect
 	github.com/evanphx/json-patch v5.6.0+incompatible // indirect
 	github.com/evanphx/json-patch/v5 v5.6.0 // indirect
-	github.com/fluxcd/pkg/apis/meta v0.19.0 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/zapr v1.2.3 // indirect
@@ -39,7 +40,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
-	github.com/onsi/ginkgo/v2 v2.7.0 // indirect
+	github.com/onsi/gomega v1.26.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/prometheus/client_golang v1.14.0 // indirect
 	github.com/prometheus/client_model v0.3.0 // indirect
