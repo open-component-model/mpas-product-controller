a71ddb735d6b5d0e4b8dd373210288f7f9c5739d
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2023-04-21 12:58:12 +0200

    feat: add reconiling ProductDeploymentGenerator

---
+++ api/v1alpha1/condition_types.go
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: 2022 SAP SE or an SAP affiliate company and Open Component Model contributors.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package v1alpha1
+
+const (
+	// PatchFailedReason is used when we couldn't patch an object.
+	PatchFailedReason = "PatchFailed"
+
+	// ComponentSubscriptionGetFailedReason is used when the subscription object cannot be found.
+	ComponentSubscriptionGetFailedReason = "ComponentSubscriptionGetFailed"
+)
--- api/v1alpha1/productdeploymentgenerator_types.go
+++ api/v1alpha1/productdeploymentgenerator_types.go
@@ -5,11 +5,28 @@
 package v1alpha1
 
 import (
+	"time"
+
+	"github.com/fluxcd/pkg/apis/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+// RepositoryRef represents the name of a repository.
+type RepositoryRef struct {
+	Name string `json:"name"`
+}
+
 // ProductDeploymentGeneratorSpec defines the desired state of ProductDeploymentGenerator
 type ProductDeploymentGeneratorSpec struct {
+	// Interval is the reconciliation interval, i.e. at what interval shall a reconciliation happen.
+	// This is used to requeue objects for reconciliation in case the related subscription hasn't been finished yet.
+	// +required
+	Interval metav1.Duration `json:"interval"`
+
+	SubscriptionRef meta.NamespacedObjectReference `json:"subscriptionRef"`
+
+	//+optional
+	RepositoryRef *RepositoryRef `json:"repositoryRef,omitempty"`
 }
 
 // ProductDeploymentGeneratorStatus defines the observed state of ProductDeploymentGenerator
@@ -32,6 +49,12 @@ func (in *ProductDeploymentGenerator) GetConditions() []metav1.Condition {
 // SetConditions sets the conditions of the ComponentVersion.
 func (in *ProductDeploymentGenerator) SetConditions(conditions []metav1.Condition) {
 	in.Status.Conditions = conditions
+}
+
+// GetRequeueAfter returns the duration after which the ComponentVersion must be
+// reconciled again.
+func (in ProductDeploymentGenerator) GetRequeueAfter() time.Duration {
+	return in.Spec.Interval.Duration
 }
 
 //+kubebuilder:object:root=true
