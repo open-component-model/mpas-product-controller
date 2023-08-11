35994dfba4a71c907353ca58304a02d20842382e
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2023-08-11 13:15:37 +0200

    fix: the target namespace usage and small typo (#35)
    * fix: the target namespace usage and small typo
    
    * added fallback to mpas-system namespace

--- controllers/select_target.go
+++ controllers/select_target.go
@@ -12,28 +12,30 @@ import (
 
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	"github.com/open-component-model/mpas-product-controller/api/v1alpha1"
 )
 
 // SelectTarget selects a target based on the provided filtering options.
 func (r *ProductDeploymentPipelineScheduler) SelectTarget(ctx context.Context, role v1alpha1.TargetRole, namespace string) (v1alpha1.Target, error) {
-	targetList := &v1alpha1.TargetList{}
+	logger := log.FromContext(ctx)
 
-	m, err := v1.LabelSelectorAsSelector(&role.Selector)
+	logger.Info("searching for targets in namespace", "namespace", namespace)
+	targetList, err := r.searchForTargetsInNamespace(ctx, role.Selector, namespace)
 	if err != nil {
-		return v1alpha1.Target{}, fmt.Errorf("failed to parse label selectors to map: %w", err)
+		return v1alpha1.Target{}, fmt.Errorf("failed to search for targets in namespace %s: %w", namespace, err)
 	}
 
-	// We can't use client.MatchingFields for spec.type since we don't have a controller for Target
-	// and thus, we can't index the type field.
-	//TODO: post MVP add controller for targets
-	if err := r.List(ctx, targetList, client.InNamespace(namespace), client.MatchingLabelsSelector{
-		Selector: m,
-	}); err != nil {
-		return v1alpha1.Target{}, fmt.Errorf("failed to list targets: %w", err)
+	if len(targetList.Items) == 0 {
+		logger.Info("no targets found in given namespace; searching for targets in mpas-system")
+		targetList, err = r.searchForTargetsInNamespace(ctx, role.Selector, r.MpasSystemNamespace)
+		if err != nil {
+			return v1alpha1.Target{}, fmt.Errorf("failed to search for targets in mpas system namespace: %w", err)
+		}
 	}
 
+	logger.Info("filtering targets based on criteria")
 	var targets []v1alpha1.Target
 	for _, target := range targetList.Items {
 		if target.Spec.Type == role.Type {
@@ -51,4 +53,23 @@ func (r *ProductDeploymentPipelineScheduler) SelectTarget(ctx context.Context, r
 		index := r.Intn(len(targets))
 		return targets[index], nil
 	}
+}
+
+func (r *ProductDeploymentPipelineScheduler) searchForTargetsInNamespace(ctx context.Context, selector v1.LabelSelector, namespace string) (*v1alpha1.TargetList, error) {
+	targetList := &v1alpha1.TargetList{}
+	m, err := v1.LabelSelectorAsSelector(&selector)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse label selectors to map: %w", err)
+	}
+
+	// We can't use client.MatchingFields for spec.type since we don't have a controller for Target
+	// and thus, we can't index the type field.
+	//TODO: post MVP add controller for targets
+	if err := r.List(ctx, targetList, client.InNamespace(namespace), client.MatchingLabelsSelector{
+		Selector: m,
+	}); err != nil {
+		return nil, fmt.Errorf("failed to list targets: %w", err)
+	}
+
+	return targetList, nil
 }
